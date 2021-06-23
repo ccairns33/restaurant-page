f5e97ae23292f158e82f5e7e24608576dfa15a2f
Carla <ccairns1999@gmail.com>
2021-06-22 22:32:20 -0500

    nav-bar added

--- src/website.js
+++ src/website.js
@@ -12,10 +12,35 @@ let createHeader = () =>{
     return header;
 
 }
+let createNav = () =>{
+    const navBar = document.createElement("nav");
 
+    const tabDiv = document.createElement("div");
+    tabDiv.classList.add("tabs" ,"d-flex","center");
+    
+    const welcomeBtn = document.createElement("button");
+    welcomeBtn.textContent="Benvenuto!";
+    welcomeBtn.classList.add("nav-btn");
+    tabDiv.appendChild(welcomeBtn);
+
+    const menu = document.createElement("button");
+    menu.textContent="Menu";
+    menu.classList.add("nav-btn");
+    tabDiv.appendChild(menu);
+
+    const contact = document.createElement("button");
+    contact.textContent="Contact";
+    contact.classList.add("nav-btn", "blue");
+    tabDiv.appendChild(contact);
+
+    navBar.appendChild(tabDiv);
+
+    return navBar;
+}
 let initWebsite = () =>{
     const content = document.querySelector(".content");
     content.appendChild(createHeader());
+    content.appendChild(createNav());
 }
 
 
